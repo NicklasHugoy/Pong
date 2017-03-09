6aac45c3b61cabc7a1feb178418cbe965e568be3
Nicklashug <Nicklas.hug@gmail.com>
2017-03-09 15:27:08 +0100

    Added assets and movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float speed = 0.2f;
+	private float input = 0.5f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		transform.position =  new Vector2(transform.position.x, Mathf.Lerp(5,-5, Input.GetAxis("Vertical")));
+
+		Debug.Log(Input.GetAxis("Vertical"));
+
+
+	}
+}
